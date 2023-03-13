61a1cd9f64631f77ba19caf721f35036307de3bc
Cyril-777 <kyrillosmaher10@gmail.com>
2023-03-13 09:04:38 -0700

    delete at func

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
+    length = len(my_list)
+
+    if idx < 0 or idx >= length:
         return (my_list)
-    result = []
-    for i in range(len(my_list)):
-        if i != idx:
-            result.append(my_list[i])
-    return (result)
+
+    del my_list[idx]
+
+    return (my_list)
