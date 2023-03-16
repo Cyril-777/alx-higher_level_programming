85cb9ce1bf624cfe00688018c1104944da443c58
Cyril-777 <kyrillosmaher10@gmail.com>
2023-03-15 17:12:18 -0700

    delete keys with spec value func

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    return {key: val for key, val in a_dictionary.items() if val != value}
+    while value in a_dictionary.values():
+        for i, x in a_dictionary.items():
+            if x == value:
+                del a_dictionary[i]
+                break
+
+    return (a_dictionary)
