688be6b275a4a834efec6cee1f5a782b84468955
Cyril-777 <kyrillosmaher10@gmail.com>
2023-04-12 18:41:02 -0700

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""a class Student that defines a student"""
+
+
+class Student:
+    """student class class"""
+
+    def __init__(self, first_name, last_name, age):
+        """ Instantiation """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is not None and all(isinstance(a, str) for a in attrs):
+            return {
+                a: getattr(self, a) for a in attrs if hasattr(self, a)
+            }
+        else:
+            return self.__dict__.copy()
