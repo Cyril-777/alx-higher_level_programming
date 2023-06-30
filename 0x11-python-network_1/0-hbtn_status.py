d0b45a015f50fe7d2f5569e07375e14d28002eb1
Cyril-777 <kyrillosmaher10@gmail.com>
2023-06-30 13:15:08 -0700

    if name = main added

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,10 +5,11 @@ import urllib.request
 
 url = "https://alx-intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
+if __name__ == "__main__":
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
 
-    print("Body response:")
-    print("    - type:", type(body))
-    print("    - content:", body)
-    print("    - utf8 content:", body.decode('utf-8'))
+        print("Body response:")
+        print("    - type:", type(body))
+        print("    - content:", body)
+        print("    - utf8 content:", body.decode('utf-8'))
